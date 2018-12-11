9e359a2ab38b8e8ea9a905ed6ea29503ea9e0b19
ikaiguang <ikaiguang@163.com>
2018-12-11 02:41:03 +0000

    go-dblink

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ikaiguang/go-dblink
+
+require (
+	github.com/go-sql-driver/mysql v1.4.0 // indirect
+	github.com/jinzhu/gorm v1.9.1
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+)
---
+++ mysql/conn_test.go
@@ -0,0 +1,25 @@
+package mysql
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDBConn(t *testing.T) {
+	os.Setenv("MysqlDebug", "true")
+	os.Setenv("MysqlTablePrefix", "ikg_")
+	os.Setenv("MysqlUser", "root")
+	os.Setenv("MysqlPassword", "Mysql.123456")
+	os.Setenv("MysqlNet", "tcp")
+	os.Setenv("MysqlAddr", "127.0.0.1:3306")
+	os.Setenv("MysqlName", "test")
+	os.Setenv("MysqlParameters", "charset=utf8mb4&collation=utf8_general_ci&timeout=60s&loc=Local&autocommit=true")
+	os.Setenv("MysqlCollation", "utf8_general_ci")
+	os.Setenv("MysqlMaxOpenConn", "0")
+	os.Setenv("MysqlMaxIdleConn", "0")
+	os.Setenv("MysqlConnMaxLifetime", "0s")
+
+	if err := NewDBConn().DB().Ping(); err != nil {
+		t.Errorf("testing : NewDBConn().DB().Ping() error : %v", err)
+	}
+}
